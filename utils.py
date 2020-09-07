a4e3ee5226e675e2165dd33064a85def5be391df
jogamaral <69370411+jogamaral@users.noreply.github.com>
2020-09-06 23:26:25 -0300

    adicionando arquivos

---
+++ utils.py
@@ -0,0 +1,8 @@
+tw_dict = {'created_at':[],
+               'id':[],
+               'id_str':[],
+               'full_text':[],
+               'entities':[],
+               'source':[],
+               'user':[],
+               'lang':[]}
