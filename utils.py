0c143dde51e9369b3189de61ff1a013965b31911
jogamaral <69370411+jogamaral@users.noreply.github.com>
2020-09-07 12:53:54 -0300

    usando pickle para salvar modelo

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import nltk
+
 tw_dict = {'created_at':[],
                'id':[],
                'id_str':[],
@@ -6,3 +8,12 @@ tw_dict = {'created_at':[],
                'source':[],
                'user':[],
                'lang':[]}
+
+def Preprocessing(instancia):
+    # Remove caracteres indesejados.
+    instancia = re.sub(r"#\S+", "", instancia)
+    instancia = re.sub(r"@\S+", "", instancia).lower().replace('.','').replace(';','').replace('-','').replace(':','').replace(')','').replace('"','').replace(',','')
+    # Removendo palavras e termos frequentes que não tem relevância nos dados.
+    stopwords = set(nltk.corpus.stopwords.words('portuguese'))
+    palavras = [i for i in instancia.split() if not i in stopwords]
+    return (" ".join(palavras))
